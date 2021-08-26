684cc2aaa243d403be40cfda1dee8f916e9b40a7
duha <dad9280@gmail.com>
2021-08-26 14:50:26 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+//my comment duha
+namespace myapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
